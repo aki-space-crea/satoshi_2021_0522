19527330ecf0af28325268ae577afcb782ab8227
aki-space-crea <aki.chan.w.13@gmail.com>
2021-05-29 14:33:38 +0900

    画像を拡大表示できるjsを追加。

--- src/index.js
+++ src/index.js
@@ -2,20 +2,21 @@ import jQuery from "jquery";
 // ドルマークに参照を代入(慣習的な $ を使うため)
 const $ = jQuery;
 
-import './style.scss';
-import { togggleMenu } from './js/_menu';
-import { scrollAnimation } from './js/_scrollFadeAnimation';
-import { clickedToggle } from './js/_clickedToggle';
-import { loading } from './js/_loading';
+import "./style.scss";
+import { togggleMenu } from "./js/_menu";
+import { scrollAnimation } from "./js/_scrollFadeAnimation";
+import { clickedToggle } from "./js/_clickedToggle";
+import { loading } from "./js/_loading";
+import { scaleupImg } from "./js/_scaleup-img";
 
 // font-awesome
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
-import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
 library.add(fas, far, fab);
 dom.i2svg();
@@ -24,21 +25,23 @@ loading();
 togggleMenu();
 scrollAnimation();
 clickedToggle();
+scaleupImg();
+objectFitImages();
 
 // コアバージョンに加えて、ナビゲーションとページネーションを追加する
-import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+import SwiperCore, { Navigation, Pagination } from "swiper/core";
 
 // モジュールを使用可能に
 SwiperCore.use([Navigation, Pagination]);
 
-const swiper = new SwiperCore('.js-slider1', {
+new SwiperCore(".js-slider1", {
   // Optional parameters
   // 方向
-  direction: 'horizontal',
+  direction: "horizontal",
   // ループ
   loop: true,
   // 切り替え時のアニメーション
-  effect: 'fade',
+  effect: "fade",
   // 表示位置
   centeredSlides: true,
   // 余白
@@ -48,38 +51,37 @@ const swiper = new SwiperCore('.js-slider1', {
   breakpoints: {
     // 画面幅が769px以上の設定
     1024: {
-      slidesPerView: 3,
+      slidesPerView: 3
     }
   },
 
   // If we need pagination
   // ページャー
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination"
   },
 
   // Navigation arrows
   // 進む、戻るボタン
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
   },
 
   // And if we need scrollbar
   scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+    el: ".swiper-scrollbar"
+  }
 });
 
-
-const swiper2 = new SwiperCore('.js-slider2', {
+new SwiperCore(".js-slider2", {
   // Optional parameters
   // 方向
-  direction: 'horizontal',
+  direction: "horizontal",
   // ループ
   loop: true,
   // 切り替え時のアニメーション
-  effect: 'fade',
+  effect: "fade",
   // 表示位置
   centeredSlides: true,
   // 余白
@@ -89,25 +91,25 @@ const swiper2 = new SwiperCore('.js-slider2', {
   breakpoints: {
     // 画面幅が769px以上の設定
     1024: {
-      slidesPerView: 3,
+      slidesPerView: 3
     }
   },
 
   // If we need pagination
   // ページャー
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination"
   },
 
   // Navigation arrows
   // 進む、戻るボタン
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
   },
 
   // And if we need scrollbar
   scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+    el: ".swiper-scrollbar"
+  }
 });
